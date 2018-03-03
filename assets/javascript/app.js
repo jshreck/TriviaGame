46e945ed448f539ae688f0c6db7e57b3b1401f30
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-03-03 00:00:36 -0500

    rotating through questions, timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,48 +21,94 @@ var questionOptions = [
 var numberCorrect = 0;
 var gameQuestions = [];
 var questionIndex = 0;
+var questionTimer = {
+    timerDuration: 30,
+    timeLeft: 30,
+    countdown: function () {
+        if (questionTimer.timeLeft===0){
+            questionTimer.stop();
+            outOfTime();
+        }
+        else {
+        questionTimer.timeLeft--;
+        $(".progress-bar").css("width", (questionTimer.timeLeft/questionTimer.timerDuration * 100) + "%").html(questionTimer.timeLeft);
+        }
+    },
+    start: function () {
+        questionTimer.timeLeft = questionTimer.timerDuration;
+        timer = setInterval(questionTimer.countdown, 1000);
+    },
+    stop: function () {
+        clearInterval(timer);
+    }
+}
 
-//Pick X random questions used for game
+//SHOULD THIS BE PART OF OBJECT?
+function outOfTime () {
+    console.log("Out of time!");
+    nextQuestion();
+}
+
+//Pick random questions used for game
 for (i = 0; i < 2; i++) {
-    var index = Math.floor(Math.random()*questionOptions.length);
+    var index = Math.floor(Math.random() * questionOptions.length);
     var question = questionOptions[index];
     gameQuestions.push(question);
     questionOptions.splice(index, 1);
 }
-//CHECK to see what questions are picked
-console.log(gameQuestions);
 
-//display questions and it's answer choices
-function display (question) {
-$("#question").html(gameQuestions[question].question);
-var answerChoiceCounter = 0;
-$(".well").each(function () {
-    $(this).html(gameQuestions[question].answerChoices[answerChoiceCounter]);
-    answerChoiceCounter ++;
-});
+//Prep for nextQuestion
+function nextQuestion() {
+    questionIndex++;
+    setTimeout(function () {
+        $(".well").removeClass("answerChoice");
+        display();
+    }, 2000);
 }
+
+//display questions and answer choices
+function display() {
+    $(".progress-bar").css("width", "100%");
+    questionTimer.start();
+    if (questionIndex < gameQuestions.length) {
+    $("#question-number").html("Question " + (questionIndex+1));    
+    $("#question").html(gameQuestions[questionIndex].question);
+        var answerChoiceCounter = 0;
+        $(".well").each(function () {
+            $(this).html(gameQuestions[questionIndex].answerChoices[answerChoiceCounter]);
+            answerChoiceCounter++;
+        });
+    }
+    else {
+        console.log("GAME OVER");
+        questionTimer.stop();
+        //DISPLAY FINAL SCREEN
+    }
+}
+
 // function to check game status to see if @ end, check before displaying if not then display and if so then "final screen"
 //play again function? either refresh page or restore original questions options array and pick new questions + reset score
 
 
-    
- display (questionIndex);
- //When answer is chosen, check to see if it's right
- $(".well").on("click", function() {
+//===========================================================================
+display();
+$("#total-questions").html(gameQuestions.length);
+
+//When answer is chosen, check to see if it's right, then go to the next question
+$(".well").on("click", function () {
+    questionTimer.stop();
+    $(this).addClass("answerChoice");
     var answerPicked = $(this).html();
     if (answerPicked === gameQuestions[questionIndex].correctAnswer) {
-        console.log ("right answer");
+        numberCorrect++; 
+        $("#numberCorrect").html(numberCorrect);  
+        console.log("right answer");
     }
     else {
-        console.log ("wrong answer");
+        console.log("wrong answer");
     }
-    questionIndex ++;
-    display (questionIndex);
-    });
+    nextQuestion();
+});
 
-// click to start, will go to question 1
-// when question answer picked, say right or wrong and if wrong display correct answer
-// will then go to next question (call display)
-// ... and at each is a timer, if it reaches the end it counts as wrong (make timer in display?)
 
 
