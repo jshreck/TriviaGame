2250bc12e993db2d5519d31fabd31c6a1c3e74bd
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-03-05 23:05:41 -0500

    styled, 25 questions to choose from

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,8 +28,8 @@ var questionOptions = [
 
     {
         question: "What color is Cinderella's original dress (the one ripped apart by her stepsisters)?",
-        answerChoices: ["blue","pink","purple","green"],
-        correctAnswer: "pink"
+        answerChoices: ["Blue","Pink","Purple","Green"],
+        correctAnswer: "Pink"
     },
 
     {
@@ -39,14 +39,14 @@ var questionOptions = [
     },
     {
         question: "In Brave, what is Merida skilled in?",
-        answerChoices: ["baking","stone throwing","knitting","archery"],
-        correctAnswer: "archery"
+        answerChoices: ["Baking","Stone throwing","Knitting","Archery"],
+        correctAnswer: "Archery"
     },
 
     {
         question: "In Alice in Wonderland, what occasion was being celebrated at The Mad Tea Party?",
-        answerChoices: ["tea time","a birthday","an unbirthday","the arrival of summer"],
-        correctAnswer: "an unbirthday"
+        answerChoices: ["Tea time","A birthday","An unbirthday","The arrival of summer"],
+        correctAnswer: "An unbirthday"
     },
 
     {
@@ -58,11 +58,80 @@ var questionOptions = [
         question: "What country is the movie Beauty in the Beast set in?",
         answerChoices: ["France","Italy","Spain","Canada"],
         correctAnswer: "France"
+    },
+    {
+        question: "In Hercules, what is the color of Hades's hair?",
+        answerChoices: ["Orange","Black","Blue","Red"],
+        correctAnswer: "Blue"
+    }, {
+        question: "What is the main character's name in Holes?",
+        answerChoices: ["Michael Leahcim","Stanley Yelnats","Marcus Sucram","Daniel Leinad"],
+        correctAnswer: "Stanley Yelnats"
+    }, {
+        question: "What meal do the Lady and the Tramp share on their date?",
+        answerChoices: ["Pierogies","Baked ziti","Table scraps","Spaghetti"],
+        correctAnswer: "Spaghetti"
+    }, {
+        question: "What was the name of Bambi's skunk friend?",
+        answerChoices: ["Rose","Flower","Stinker","Oreo"],
+        correctAnswer: "Flower"
+    }, {
+        question: "In meet the Robinsons, what animals does Franny teach to sing?",
+        answerChoices: ["Frogs","Birds","Cat","Lizards"],
+        correctAnswer: "Frogs"
+    }, {
+        question: "What is most energy-producing resource for Monsters Inc?",
+        answerChoices: ["Screams","Laughter","Snoring","Crying"],
+        correctAnswer: "Laughter"
+    }, {
+        question: "What is the name of Mulan's pet dragon?",
+        answerChoices: ["Li Shang","Abdul","Mushu","Dhanush"],
+        correctAnswer: "Mushu"
+    }, {
+        question: "What is the raccoon's name in Pocahantas?",
+        answerChoices: ["Charlie","Powhatan","John Smith","Meeko"],
+        correctAnswer: "Meeko"
+    }, {
+        question: "Which language does Kronk speak in the Emperor's New Groove?",
+        answerChoices: ["Squirrel","Spanish","Portugese","Gibberish"],
+        correctAnswer: "Squirrel"
+    }, {
+        question: "What is the name of the festival in The Hunchback of Notre Dame?",
+        answerChoices: ["Festival of Fools","Party for Paupers","Festival of Food","Gathering of Goons"],
+        correctAnswer: "Festival of Fools"
+    }, {
+        question: "In The Incredibles, which part of a costume does Edna warn against?",
+        answerChoices: ["Mask","Boots","Cape","Gloves"],
+        correctAnswer: "Cape"
+    }, {
+        question: "What was the name of Bambi's skunk friend?",
+        answerChoices: ["Rose","Flower","Stinker","Oreo"],
+        correctAnswer: "Flower"
+    }, {
+        question: "What is meaning of Hakuna Matata?",
+        answerChoices: ["No worries","Whatever","It's cool","No problem"],
+        correctAnswer: "No worries"
+    }, {
+        question: "What does Ariel, the little mermaid, think a fork is used for?",
+        answerChoices: ["Brushing hair","Throwing things","Scratching an itch","Digging"],
+        correctAnswer: "Brushing hair"
+    }, {
+        question: "In the beginning of WALL-E, what tool does he have trouble sorting?",
+        answerChoices: ["Screwdriver","Can opener","Spork","Level"],
+        correctAnswer: "Spork"
+    }, {
+        question: "What is Andy's neighbor's name in Toy Story?",
+        answerChoices: ["Mark","Sid","Karl","Anthony"],
+        correctAnswer: "Sid"
+    }, {
+        question: "What is Donald Duck's middle name?",
+        answerChoices: ["Bellatrix","Maximilian","Heston","Fauntleroy"],
+        correctAnswer: "Fauntleroy"
     }
 ];
 //setting up timer
 var questionTimer = {
-    timerDuration: 5, //set timer duration here
+    timerDuration: 10, //set timer duration here
     timeLeft: this.timerDuration, //using this to countdown
     countdown: function () {
         if (questionTimer.timeLeft===0){
@@ -112,9 +181,7 @@ function display() {
         });
     }
     else {
-        $(".well").addClass("disabled");
         questionTimer.stop();
-        console.log("GAME OVER");
         $(".container").append('<div class="game-over">');
         $(".game-over").height($(".container").height());
         $(".game-over").append("<h1>GAME OVER</h1>");
@@ -131,7 +198,7 @@ function display() {
 
 //===========================================================================
 //Pick random questions used for game
-for (i = 0; i < 3; i++) {
+for (i = 0; i < 5; i++) {
     var index = Math.floor(Math.random() * questionOptions.length);
     var question = questionOptions[index];
     gameQuestions.push(question);
@@ -154,7 +221,7 @@ $(".well").on("click", function () {
         $(".message").html( "Correct! Answer: " + gameQuestions[questionIndex].correctAnswer + " .");
     }
     else {
-        $(".message").html( "Incorrect. The correct answer was " + gameQuestions[questionIndex].correctAnswer + " .");
+        $(".message").html( "Incorrect. The correct answer is " + gameQuestions[questionIndex].correctAnswer + " .");
     }
     nextQuestion();
 });
